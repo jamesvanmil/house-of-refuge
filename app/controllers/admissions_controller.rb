08323479868dfe078b631b367c1d35b7b718110a
James Van Mil <james.vanmil@gmail.com>
2015-03-05 16:44:36 -0500

    Remove un-needed controller scaffolding

--- app/controllers/admissions_controller.rb
+++ app/controllers/admissions_controller.rb
@@ -1,5 +1,5 @@
 class AdmissionsController < ApplicationController
-  before_action :set_admission, only: [:show, :edit, :update, :destroy]
+  before_action :set_admission, only: [:show]
 
   # GET /admissions
   # GET /admissions.json
@@ -10,55 +10,6 @@ class AdmissionsController < ApplicationController
   # GET /admissions/1
   # GET /admissions/1.json
   def show
-  end
-
-  # GET /admissions/new
-  def new
-    @admission = Admission.new
-  end
-
-  # GET /admissions/1/edit
-  def edit
-  end
-
-  # POST /admissions
-  # POST /admissions.json
-  def create
-    @admission = Admission.new(admission_params)
-
-    respond_to do |format|
-      if @admission.save
-        format.html { redirect_to @admission, notice: 'Admission was successfully created.' }
-        format.json { render :show, status: :created, location: @admission }
-      else
-        format.html { render :new }
-        format.json { render json: @admission.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /admissions/1
-  # PATCH/PUT /admissions/1.json
-  def update
-    respond_to do |format|
-      if @admission.update(admission_params)
-        format.html { redirect_to @admission, notice: 'Admission was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admission }
-      else
-        format.html { render :edit }
-        format.json { render json: @admission.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /admissions/1
-  # DELETE /admissions/1.json
-  def destroy
-    @admission.destroy
-    respond_to do |format|
-      format.html { redirect_to admissions_url, notice: 'Admission was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
--- spec/controllers/admissions_controller_spec.rb
+++ spec/controllers/admissions_controller_spec.rb
@@ -20,17 +20,6 @@ require 'rails_helper'
 
 RSpec.describe AdmissionsController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Admission. As you add validations to Admission, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # AdmissionsController. Be sure to keep this updated too.
@@ -38,7 +27,7 @@ RSpec.describe AdmissionsController, type: :controller do
 
   describe "GET #index" do
     it "assigns all admissions as @admissions" do
-      admission = Admission.create! valid_attributes
+      admission = Admission.create!
       get :index, {}, valid_session
       expect(assigns(:admissions)).to eq([admission])
     end
@@ -46,114 +35,9 @@ RSpec.describe AdmissionsController, type: :controller do
 
   describe "GET #show" do
     it "assigns the requested admission as @admission" do
-      admission = Admission.create! valid_attributes
+      admission = Admission.create!
       get :show, {:id => admission.to_param}, valid_session
       expect(assigns(:admission)).to eq(admission)
     end
   end
-
-  describe "GET #new" do
-    it "assigns a new admission as @admission" do
-      get :new, {}, valid_session
-      expect(assigns(:admission)).to be_a_new(Admission)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested admission as @admission" do
-      admission = Admission.create! valid_attributes
-      get :edit, {:id => admission.to_param}, valid_session
-      expect(assigns(:admission)).to eq(admission)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Admission" do
-        expect {
-          post :create, {:admission => valid_attributes}, valid_session
-        }.to change(Admission, :count).by(1)
-      end
-
-      it "assigns a newly created admission as @admission" do
-        post :create, {:admission => valid_attributes}, valid_session
-        expect(assigns(:admission)).to be_a(Admission)
-        expect(assigns(:admission)).to be_persisted
-      end
-
-      it "redirects to the created admission" do
-        post :create, {:admission => valid_attributes}, valid_session
-        expect(response).to redirect_to(Admission.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved admission as @admission" do
-        post :create, {:admission => invalid_attributes}, valid_session
-        expect(assigns(:admission)).to be_a_new(Admission)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:admission => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested admission" do
-        admission = Admission.create! valid_attributes
-        put :update, {:id => admission.to_param, :admission => new_attributes}, valid_session
-        admission.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested admission as @admission" do
-        admission = Admission.create! valid_attributes
-        put :update, {:id => admission.to_param, :admission => valid_attributes}, valid_session
-        expect(assigns(:admission)).to eq(admission)
-      end
-
-      it "redirects to the admission" do
-        admission = Admission.create! valid_attributes
-        put :update, {:id => admission.to_param, :admission => valid_attributes}, valid_session
-        expect(response).to redirect_to(admission)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the admission as @admission" do
-        admission = Admission.create! valid_attributes
-        put :update, {:id => admission.to_param, :admission => invalid_attributes}, valid_session
-        expect(assigns(:admission)).to eq(admission)
-      end
-
-      it "re-renders the 'edit' template" do
-        admission = Admission.create! valid_attributes
-        put :update, {:id => admission.to_param, :admission => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested admission" do
-      admission = Admission.create! valid_attributes
-      expect {
-        delete :destroy, {:id => admission.to_param}, valid_session
-      }.to change(Admission, :count).by(-1)
-    end
-
-    it "redirects to the admissions list" do
-      admission = Admission.create! valid_attributes
-      delete :destroy, {:id => admission.to_param}, valid_session
-      expect(response).to redirect_to(admissions_url)
-    end
-  end
-
 end
