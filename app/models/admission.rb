5146016f2908dc574ff7ddf8cd999659d303f725
James Van Mil <james.vanmil@gmail.com>
2015-03-15 15:30:24 -0400

    Add Sunspot functionality to models

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,3 +1,12 @@
 class Admission < ActiveRecord::Base
+  ## Sunspot search
+  searchable do
+    text  :by_whom_committed, :name, :alias, :for_what_committed, :disposal, :whereborn_city, :whereborn_state, :whereborn_country, :parentage
+    boolean :complaint_of_father, :complaint_of_mother, :complaint_of_police, :appearance_bad, :appearance_good, :can_read, :can_write, :father_drinks, :father_living, :had_regular_work, :has_step_father, :has_step_mother, :mother_drinks, :mother_living, :played_truant, :swears, :uses_liquour, :uses_tobacco
+    integer :age, :no_of_times_in_refuge, :history_number
+    text :no_of_reader, :gender, :whereborn_city, :whereborn_state, :whereborn_country, :religion, :parentage, :image_no
+  end
+
+  ## Pagination
   self.per_page = 10
 end
