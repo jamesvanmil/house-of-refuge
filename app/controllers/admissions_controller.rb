75f3443f00994b67c849fa3a9b6d1c997773513e
James Van Mil <james.vanmil@gmail.com>
2015-04-17 08:13:08 -0400

    Add support for multifield searching in Controller

--- app/controllers/admissions_controller.rb
+++ app/controllers/admissions_controller.rb
@@ -4,9 +4,12 @@ class AdmissionsController < ApplicationController
   # GET /admissions
   # GET /admissions.json
   def index
-    @search = Admission.search do
-      fulltext params[:search], fields: params[:field]
+    parse_search_request
 
+    @search = Admission.search do
+      params[:search].each do |search|
+        fulltext search[:value], fields: search[:field]
+      end
       paginate(per_page: 30, page: params[:page])
     end
 
@@ -19,6 +22,38 @@ class AdmissionsController < ApplicationController
   end
 
   private
+
+  def parse_search_request
+    fields = select_all_search_fields
+    values = select_all_search_values
+    set_search_fields_and_values(fields, values)
+    handle_blank_search
+  end
+
+  def set_search_fields_and_values(fields, values)
+    params[:search] = Array.new
+    counter = set_counter(fields, values)
+    counter.times do |i|
+      params[:search][i] = { field: fields["field#{i}"], value: values["value#{i}"] }
+    end
+  end
+
+  def set_counter(fields, values)
+    return fields.count if fields.count == values.count
+    raise "Field count does not match value count"
+  end
+
+  def select_all_search_fields
+    params.select { |key, value| key.to_s.match(/^field/)}
+  end
+
+  def select_all_search_values
+    params.select { |key, value| key.to_s.match(/^value/)}
+  end
+
+  def handle_blank_search
+    params[:search] = [{ value: nil, field: nil }] if params[:search].empty?
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_admission
