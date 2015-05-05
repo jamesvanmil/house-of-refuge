21ad5270caa9c0376246d3692e2c2b7c32510704
James Van Mil <james.vanmil@gmail.com>
2015-05-05 10:23:24 -0400

    Add pending test for Sunspot-dependent feature

--- spec/views/admissions/index.html.erb_spec.rb
+++ spec/views/admissions/index.html.erb_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "admissions/index", type: :view do
+  it "shows a + when a result has matching history numbers" do
+    pending ## sunspot testing
+  end
 end
