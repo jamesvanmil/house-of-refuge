3c1085037965297e44e0eeafa90ee23c93f25fd2
James Van Mil <james.vanmil@gmail.com>
2015-07-23 21:39:21 -0400

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/home'
+
   resources :admissions
 
   # The priority is based upon order of creation: first created -> highest priority.
