043b90ce801660caa4d3f9a2876241940e212bc3
James Van Mil <james.vanmil@gmail.com>
2016-04-18 11:57:08 -0400

    basic facet behavior for images

--- app/helpers/gallery_images_helper.rb
+++ app/helpers/gallery_images_helper.rb
@@ -1,7 +1,12 @@
 module GalleryImagesHelper
   def facet_link(subject)
-    link_to subject.capitalize, add_to_search(subject)
-    #link_to subject.capitalize + " (remove)", remove_from_search(subject)
+    if params["subject"].nil?
+      link_to subject.capitalize, add_to_search(subject)
+    elsif params["subject"].include?(subject) 
+      link_to subject.capitalize + " (remove)", remove_from_search(subject)
+    else
+      link_to subject.capitalize, add_to_search(subject)
+    end
   end
 
   def add_to_search(subject)
@@ -9,12 +14,14 @@ module GalleryImagesHelper
   end
 
   def remove_from_search(subject)
-    request.params.merge({ subject: params[:subject].delete(subject) })
+    subjects = params[:subject].dup
+    subjects.delete(subject)
+    request.params.merge({ subject: subjects, page: nil })
   end
 
   def merged_subjects(subject)
     if params[:subject].present?
-      [params[:subject]] + [subject]
+      params[:subject] + [subject]
     else
       [subject]
     end
