965554bd7dba75e968bf5f6962391463f09e75c0
James Van Mil <james.vanmil@gmail.com>
2016-09-07 14:48:22 -0400

    outline admission searcher spec

---
+++ spec/lib/admissions_searcher_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe AdmissionSearcher do
+  it 'requires search_params to initialize' do
+
+  end
+
+  describe '#results' do
+    it 'should query Admissions with name' do
+      
+    end
+
+    it 'should query Admissions with other search params' do
+      
+    end
+
+    it 'should query Admissions with paginate' do
+      
+    end
+
+    it 'should paginate with page' do
+      
+    end
+
+    context 'pagination set to true' do
+      it 'should paginate with per_page set to default' do
+
+      end
+    end
+
+    context 'pagination set to false' do
+      it 'should paginate with per_page equal to item count' do
+
+      end
+    end
+  end
+end
